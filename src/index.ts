5364b665e84da3fe3f8ad88e601d285f17aceae9
damianhaq <damianhaq@gmail.com>
2023-03-09 21:35:39 +0100

    webpack test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 export const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-export const c = canvas.getContext("2d");
+export const c = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 console.log("asd");
+
+function animate(deltaTime: number) {
+  c.rect(0, 0, 50, 50);
+  c.stroke();
+}
+
+requestAnimationFrame(animate);
